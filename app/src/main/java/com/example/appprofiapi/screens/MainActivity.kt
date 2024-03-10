4333ca6e95a8a3a98b1e41f76d1a5c0916ef7ede
Otter <teo2005@bk.ru>
2024-03-10 23:29:32 +0300

    я не знаю что произошло

--- app/src/main/java/com/example/appprofiapi/screens/MainActivity.kt
+++ app/src/main/java/com/example/appprofiapi/screens/MainActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appprofiapi.R
+import com.example.appprofiapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //создаем переменную для биндинга
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
